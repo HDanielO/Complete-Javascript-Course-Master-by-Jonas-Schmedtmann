a60d425125d428a7a50a6deed57d58c8a881c3f0
HDanielO <hameeddnl@gmail.com>
2025-02-09 05:32:15 +0100

    implemented new game functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 // SELECT  ELEMENTS
 // the 'EL' signifies DOM element
 const score0EL = document.getElementById('score--0');
@@ -40,17 +39,13 @@ const rollDice = () => {
   if (isPlaying) {
     // 1. GENERATE A RANDOM NUMBER BETWEEN 1 AND 6
     const dice = Math.trunc(Math.random() * 6) + 1;
-
     // 2. DISPLAY DICE IMAGE
     diceEl.src = `dice-${dice}.png`;
     diceEl.classList.remove('hidden');
-    console.log(dice);
-
     // CHECK IF ROLLED 1
     if (dice !== 1) {
       // 3. ADD THE DICE NUMBER TO CURRENT SCORE
       currentScore += dice;
-
       // 4. DISPLAY CURRENT SCORE TO ACTIVE PLAYER
       document.querySelector(`#current--${activePlayer}`).textContent =
         currentScore;
@@ -81,4 +76,25 @@ const holdScore = () => {
       switchPlayer();
     }
   }
+};
+
+const newGame = () => {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--active');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  score0EL.textContent = 0;
+  score1EL.textContent = 0;
+  currentScore = 0;
+  scores = [0, 0];
+  activePlayer = 0;
+  isPlaying = true;
+  // hide the dice
+  diceEl.classList.add('hidden');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
 };
