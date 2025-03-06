5be241a1d66593c5dbef81e20a286da974998e77
HDanielO <hameeddnl@gmail.com>
2025-03-06 17:38:38 +0100

    learnt about enhanced object literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -478,5 +478,31 @@ console.log(allAuthors);
 //ASSIGNMENT 8.3
 
 for (const [index, element] of allAuthors.entries()) {
-  console.log(`${index+1}. ${element}`);
+  console.log(`${index + 1}. ${element}`);
 }
+
+//ASSIGNMENT 9.1
+
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[2][1],
+  [bookData[2][0]]: bookData[2][1],
+  // ...
+};
+
+// ASSIGNMENT 9.2
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages,
+  // ...
+};
